02bb0b35efff33250e87f91fc2a2f69472b3e9db
justtaaa <taya.nekrasova003@gmail.com>
2024-02-26 20:25:12 +0000

    #Added Tetris .h and .cpp files

---
+++ include/Tetris.h
@@ -0,0 +1,8 @@
+//
+// Created by Kevin Cuni on 26/02/2024.
+//
+
+#ifndef HERO_OF_THE_ARCADE_TETRIS_H
+#define HERO_OF_THE_ARCADE_TETRIS_H
+
+#endif //HERO_OF_THE_ARCADE_TETRIS_H
---
+++ src/Tetris.cpp
@@ -0,0 +1,3 @@
+//
+// Created by Kevin Cuni on 26/02/2024.
+//
