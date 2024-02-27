9baf6bda262ec4ed03d2d73942f1722f54da458e
kevinCuni2004 <kevin.cuni2004@gmail.com>
2024-02-27 09:40:41 +0000

    #Added pieces

--- include/Tetris.h
+++ include/Tetris.h
@@ -1,8 +1,41 @@
 //
-// Created by kevinCuni2004 on 26/02/2024. Test
+// Created by kevinCuni2004 on 26/02/2024.
 //
 
 #ifndef HERO_OF_THE_ARCADE_TETRIS_H
 #define HERO_OF_THE_ARCADE_TETRIS_H
+#define BOARD_HEIGHT 20
+#define BOARD_WIDTH 10
+
+//Block Pieces
+int l-block[4][4] = {0,0,0,0,
+                     0,1,0,0,
+                     0,1,0,0,
+                     0,1,1,0};
+
+int j-block[4][4] = {0,0,0,0,
+                     0,0,1,0,
+                     0,0,1,0,
+                     0,1,1,0};
+
+int o-block[4][4] = {0,0,0,0,
+                     0,1,1,0,
+                     0,1,1,0,
+                     0,0,0,0};
+
+int z-block[4][4] = {0,0,0,0,
+                     0,1,1,0,
+                     0,0,1,1,
+                     0,0,0,0};
+
+int s-block[4][4] = {0,0,0,0,
+                     0,0,1,1,
+                     0,1,1,0,
+                     0,0,0,0};
+
+int t-block[4][4] = {0,0,0,0,
+                     0,1,1,1,
+                     0,0,1,0,
+                     0,0,0,0};
 
 #endif //HERO_OF_THE_ARCADE_TETRIS_H
